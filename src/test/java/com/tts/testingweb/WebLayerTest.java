f30b77b1c2ae1aeff94f96f3ed3f89098198f0b0
Sarah Hoffman <74530173+jadedragon21@users.noreply.github.com>
2021-03-10 14:38:34 -0600

    Added WebLayerTest class in the test folder in the testingweb package. Added GreetingController class in the main folder in the testingweb package.  Weblayer is instatiating only in the weblayer rather than whole context. Added an extra component to store the greeting in the GreetingController.

---
+++ src/test/java/com/tts/testingweb/WebLayerTest.java
@@ -0,0 +1,41 @@
+package com.tts.testingweb;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.tts.testingweb.controller.HomeController;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeController.class)
+//tag::test[]
+
+
+public class WebLayerTest {
+    @BeforeEach
+    void beforeEachTest(){
+        System.out.println("Test is running....");
+    }
+    @AfterEach
+    void afterEachTest(){
+        System.out.println("Test has resolved.");
+    }
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
+    }
+
+}
+//end::test[]
