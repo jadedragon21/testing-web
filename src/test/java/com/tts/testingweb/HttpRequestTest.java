75852aaa1233aa7da6520da8def0e3444a80741f
Sarah Hoffman <74530173+jadedragon21@users.noreply.github.com>
2021-03-10 14:33:32 -0600

    Added HttpRequestTest class in the main folder in the testingweb package. It starts the app and listens for a connection and sends an Http request & asserts a response.

---
+++ src/test/java/com/tts/testingweb/HttpRequestTest.java
@@ -0,0 +1,36 @@
+package com.tts.testingweb;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+    @BeforeEach
+    void beforeEachTest(){
+        System.out.println("Test is running....");
+    }
+    @AfterEach
+    void afterEachTest(){
+        System.out.println("Test has resolved.");
+    }
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello, World");
+    }
+}
